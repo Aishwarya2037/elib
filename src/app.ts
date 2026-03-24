3a464220bc629db9f471ea3c25d368ecb5d9ec69
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-03-24 12:59:43 +0530

    add register user validation

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import type { Request, Response } from "express";
 import userRouter from "./routes/userRoute.js";
+import errorMiddleware from "./middleware/middleware.js";
 
 const app = express();
+
+app.use(express.json());
 
 // http methods:- GET, POST, PUT, PATCH, DELETE
 app.get("/", (req: Request, res: Response) => {
@@ -12,5 +15,6 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/users", userRouter);
 
 // Global error handler
+app.use(errorMiddleware);
 
 export default app;
