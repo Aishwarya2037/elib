2314e145d981b93d72bfe1ef630aceeb881724bc
Aishwarya Jagtap <aishwarya2037@gmail.com>
2026-03-24 12:37:42 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
-import type {Request, Response} from "express";
+import type { Request, Response } from "express";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 
-// http methods:- GET, POST,PUT, PATCH, DELETE
+// http methods:- GET, POST, PUT, PATCH, DELETE
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Welcome to elib apis"})
-})
+  res.json({ message: "Welcome to elib apis" });
+});
 
+app.use("/api/users", userRouter);
+
+// Global error handler
 
 export default app;
